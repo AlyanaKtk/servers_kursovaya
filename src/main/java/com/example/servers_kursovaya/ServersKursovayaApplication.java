3acf689c9f17bbbdb6921ce1f2cf98055ab3604e
Elena <catswariors2003@gmail.com>
2023-11-09 18:00:50 +0300

    Initial commit

---
+++ src/main/java/com/example/servers_kursovaya/ServersKursovayaApplication.java
@@ -0,0 +1,13 @@
+package com.example.servers_kursovaya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersKursovayaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServersKursovayaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/servers_kursovaya/ServersKursovayaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.servers_kursovaya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServersKursovayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
