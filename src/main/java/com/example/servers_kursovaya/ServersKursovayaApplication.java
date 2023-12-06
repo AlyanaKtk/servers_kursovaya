211b93fd76d1fdf003806876f14306d492e6e65e
Elena <catswariors2003@gmail.com>
2023-12-06 22:47:54 +0300

    добавлены комментарии

--- src/main/java/com/example/servers_kursovaya/ServersKursovayaApplication.java
+++ src/main/java/com/example/servers_kursovaya/ServersKursovayaApplication.java
@@ -3,11 +3,12 @@ package com.example.servers_kursovaya;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//основной класс
 @SpringBootApplication
 public class ServersKursovayaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServersKursovayaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServersKursovayaApplication.class, args);
+    }
 
 }
--- src/test/java/com/example/servers_kursovaya/ServersKursovayaApplicationTests.java
+++ src/test/java/com/example/servers_kursovaya/ServersKursovayaApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ServersKursovayaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
